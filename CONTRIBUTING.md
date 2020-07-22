93ab9b01f11df0d98bb9d795c809860b96981bb7
Michael Hargiss <mdhargiss@gmail.com>
2020-07-22 13:43:58 -0400

    use prettier for most linting

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -2,6 +2,19 @@
 
 First install the git hooks. This will save you some pull-request headaches. :)
 
-```sh
+```bash
 ./scripts/install-hooks.sh
+```
+
+We use `prettier` to format most of our files.
+
+```bash
+npm ci
+npm run lint
+```
+
+If you get any lint errors, try
+
+```bash
+npm run lint:fix
 ```
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,6 +5,5 @@ set -e
 project_root="$(dirname "$(git rev-parse --git-dir)")"
 
 bash "$project_root"/lib/continuous_integration.bash lint_dockerfiles
-bash "$project_root"/lib/continuous_integration.bash lint_markdown
 bash "$project_root"/lib/continuous_integration.bash lint_shell
-bash "$project_root"/lib/continuous_integration.bash lint_yaml
+npm run lint
