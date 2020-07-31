2acca111d32645d750627410aee59165d9925b4a
Michael Hargiss <mdhargiss@gmail.com>
2020-07-31 14:08:31 -0400

    check nightwatch version before publish

--- scripts/ci/publish_docker_image.sh
+++ scripts/ci/publish_docker_image.sh
@@ -10,6 +10,9 @@ nightwatch_version=1.3.7
 
 docker build --pull . --tag "${DOCKER_USERNAME}"/nightwatch:master
 
+(docker run --rm "${DOCKER_USERNAME}"/nightwatch:master nightwatch --version | grep "${nightwatch_version}") || \
+  (echo "The Nightwatch version and docker image tag version don't match. Aborting ..." && exit 1)
+
 docker tag "${DOCKER_USERNAME}"/nightwatch:master "${DOCKER_USERNAME}"/nightwatch:"${nightwatch_version}"
 
 docker push "${DOCKER_USERNAME}"/nightwatch:master
