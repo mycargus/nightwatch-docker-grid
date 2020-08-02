0659fcb9c17124f9b6cda694d5316041e098fc4a
Michael Hargiss <mdhargiss@gmail.com>
2020-08-01 23:44:30 -0400

    tag :latest too

--- scripts/ci/publish_docker_image.sh
+++ scripts/ci/publish_docker_image.sh
@@ -8,12 +8,14 @@ set -ev
 # TODO: get from package-lock.json
 nightwatch_version=1.3.7
 
-docker build --pull . --tag "${DOCKER_USERNAME}"/nightwatch:master
+docker build --pull . \
+  --tag "${DOCKER_USERNAME}"/nightwatch:master \
+  --tag "${DOCKER_USERNAME}"/nightwatch:"${nightwatch_version}" \
+  --tag "${DOCKER_USERNAME}"/nightwatch:latest
 
 (docker run --rm "${DOCKER_USERNAME}"/nightwatch:master nightwatch --version | grep "${nightwatch_version}") || \
   (echo "The Nightwatch version and docker image tag version don't match. Aborting ..." && exit 1)
 
-docker tag "${DOCKER_USERNAME}"/nightwatch:master "${DOCKER_USERNAME}"/nightwatch:"${nightwatch_version}"
-
 docker push "${DOCKER_USERNAME}"/nightwatch:master
 docker push "${DOCKER_USERNAME}"/nightwatch:"${nightwatch_version}"
+docker push "${DOCKER_USERNAME}"/nightwatch:latest
