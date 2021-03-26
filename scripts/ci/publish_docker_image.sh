ddc72ddfe6d3c97bd71137157369449cad3b1e2a
Michael Hargiss <mdhargiss@gmail.com>
2021-03-26 15:11:51 -0700

    upgrade nightwatch to 1.6.0; upgrade dev deps

--- scripts/ci/publish_docker_image.sh
+++ scripts/ci/publish_docker_image.sh
@@ -6,7 +6,7 @@ set -ev
   (echo "Docker login failed!" && exit 1)
 
 # TODO: get from package-lock.json
-nightwatch_version=1.5.1
+nightwatch_version=1.6.0
 
 docker build --pull . \
   --tag "${DOCKER_USERNAME}"/nightwatch:master \
